2e8d6800d74a3ddd99b8f18ed7670490e2206171
Vignesh Kalidas <vigneshk14@me.com>
2015-07-28 18:59:40 -0700

    [env] Added gcc4mbed tools to PATH

--- shell/profile.zsh
+++ shell/profile.zsh
@@ -1,4 +1,4 @@
 # vim: set ft=zsh:
 
-export PATH=/opt/local/bin:/opt/local/sbin:/usr/local/bin:$HOME/bin:$PATH
+export PATH=/opt/local/bin:/opt/local/sbin:/usr/local/bin:$HOME/bin:$HOME/bin/gcc4mbed/gcc-arm-none-eabi/bin:$PATH:
 export EDITOR=vim
