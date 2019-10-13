8c7eb5df5d4d3589e86d0fffff416d8204a5ded1
Vignesh Kalidas <vigneshk14@me.com>
2019-10-13 16:33:13 -0700

    [zsh] Don't need specific bins in path

--- shell/profile.zsh
+++ shell/profile.zsh
@@ -1,6 +1,6 @@
 # vim: set ft=zsh:
 
-export PATH=/opt/local/bin:/opt/local/sbin:/usr/local/bin:$HOME/bin:$HOME/bin/gcc4mbed/gcc-arm-none-eabi/bin:$HOME/.stack/programs/x86_64-osx/ghc-7.10.2/bin:$HOME/bin/scala/bin:$PATH:
+export PATH=/opt/local/bin:/opt/local/sbin:/usr/local/bin:$HOME/bin:$PATH:
 export EDITOR=vim
 export JAVA_HOME=$(/usr/libexec/java_home)
 
