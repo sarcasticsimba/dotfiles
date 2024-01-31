1903463c79afd98b88d6dc8ab6f66fdc90c53ae0
Vignesh <vigneshk14@me.com>
2024-01-30 20:45:47 -0800

    [func] handy notification

--- shell/func.zsh
+++ shell/func.zsh
@@ -26,3 +26,19 @@ fussh()
 {
     eval `ssh-agent -s` && ssh-add
 }
+
+# Usage: $ notify <body> <title> <subtitle>
+notify()
+{
+    if [ $# -gt 3 ]; then
+        echo "notify: More than 3 arguments provided, only using the first 3\nUsage: $ notify <body> <title> <subtitle>"
+    fi
+    # $ osascript -e 'display notification "hello world!" with title "Command Line" subtitle "Helm" sound name "Boop"'
+    body="${1:-n/a}"
+    title="${2:-CLI Notification}"
+    subtitle="${3:-notify}"
+
+    str="display notification \"$body\" with title \"$title\" subtitle \"$subtitle\" sound name \"Boop\""
+
+    osascript -e "$str"
+}
