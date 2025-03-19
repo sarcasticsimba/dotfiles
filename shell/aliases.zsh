a6dac029055db9fd55127eeba24fddbf6600da2f
Vignesh <vigneshk14@me.com>
2025-03-18 18:33:38 -0700

    [alias] tail all docker logs

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -24,10 +24,25 @@ alias wifi="networksetup -setairportpower $WIFI_PORT"
 alias wifinetwork="networksetup -getairportnetwork $WIFI_PORT"
 alias fdss="find . -name '*.DS_Store' -type f -delete -print"
 alias fuosx="sudo spctl --master-disable"
-alias dcc='docker compose'
-alias dstopped='docker ps -a --filter "status=exited" --format "{{.Names}}"'
-alias dlf='docker logs --follow'
 alias gw='./gradlew'
 alias kc='kubectl'
 alias md='vim -c :MarkdownPreview'
 alias jql='jq -C | less -R'
+
+alias dcc='docker compose'
+alias dstopped='docker ps -a --filter "status=exited" --format "{{.Names}}"'
+alias dlf='docker logs --follow'
+
+# docker ps -q → get all container IDs
+# xargs
+# | -L 1 → use 1 arg per process
+# | -P n → spawn n processes
+# |     $(docker ps -q | wc -l) → count number of containers to use as n
+# | -I replstr → define a replstr, curly braces, to replace in command
+# | bash -c "…" → run this command
+# |     docker logs --since 30s -f {} → follow logs for each container
+# awk
+# | container=foo → store a variable "container"
+# | docker inspect … | tr … → get container name from id to use as foo, trim slash
+# | print … → print the log lines prefixed with [container] in purple
+alias dlogs='docker ps -q | xargs -L 1 -P $(docker ps -q | wc -l) -I {} bash -c "docker logs --since 30s -f {} | awk -v container=\$(docker inspect --format \"{{.Name}}\" {} | tr -d \"/\") '\''{ print \"\033[35m[\" container \"]\033[0m    \" \$0 }'\''"'
