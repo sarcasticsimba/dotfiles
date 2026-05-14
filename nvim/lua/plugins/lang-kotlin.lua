49165053d2a14d9b4af754afd90939a475b2f3aa
Vignesh <vigneshk14@me.com>
2026-05-13 20:10:34 -0700

    [nvim] kotlin lsp block

--- nvim/lua/plugins/lang-kotlin.lua
+++ nvim/lua/plugins/lang-kotlin.lua
@@ -93,4 +93,13 @@ return {
       -- },
     })
   end,
+
+  {
+    "mason-org/mason.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "kotlin-lsp" })
+    end,
+  },
 }
